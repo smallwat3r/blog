b941ee13a4f3930fa697a74cbdee523e127efe1e
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2026-01-26 15:20:22 +0000

    Add TOC

--- static/blog/blog.js
+++ static/blog/blog.js
@@ -16,3 +16,79 @@ document.querySelectorAll('pre').forEach((pre) => {
     });
   });
 });
+
+// Heading anchor links
+document.querySelectorAll('article h2[id], article h3[id]').forEach((h) => {
+  const link = document.createElement('a');
+  link.href = '#' + h.id;
+  link.className = 'heading-link';
+  link.innerHTML = h.innerHTML;
+  h.textContent = '';
+  h.appendChild(link);
+
+  const pilcrow = document.createElement('span');
+  pilcrow.className = 'pilcrow';
+  pilcrow.textContent = '\u00B6';
+  link.prepend(pilcrow);
+});
+
+// Table of contents
+(function() {
+  const toc = document.querySelector('.toc');
+  const headings = document.querySelectorAll('article h2[id], article h3[id]');
+  if (!toc || headings.length < 2) return;
+
+  const title = document.createElement('div');
+  title.className = 'toc-title';
+  title.textContent = 'Contents';
+  toc.appendChild(title);
+
+  const ul = document.createElement('ul');
+  const links = [];
+  let currentSubUl = null;
+
+  headings.forEach((h) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = '#' + h.id;
+    // Get text without the pilcrow
+    const clone = h.cloneNode(true);
+    const pilcrow = clone.querySelector('.pilcrow');
+    if (pilcrow) pilcrow.remove();
+    a.textContent = clone.textContent;
+    li.appendChild(a);
+
+    if (h.tagName === 'H2') {
+      ul.appendChild(li);
+      currentSubUl = null;
+    } else {
+      li.className = 'toc-sub';
+      if (!currentSubUl) {
+        currentSubUl = document.createElement('ul');
+        currentSubUl.className = 'toc-sublist';
+        ul.appendChild(currentSubUl);
+      }
+      currentSubUl.appendChild(li);
+    }
+    links.push({ el: a, target: h });
+  });
+  toc.appendChild(ul);
+
+  // Highlight current section on scroll
+  let ticking = false;
+  function updateActive() {
+    const scrollY = window.scrollY + 100;
+    let current = links[0];
+    for (const link of links) {
+      if (link.target.offsetTop <= scrollY) current = link;
+    }
+    links.forEach((l) => l.el.classList.toggle('active', l === current));
+  }
+  window.addEventListener('scroll', () => {
+    if (!ticking) {
+      requestAnimationFrame(() => { updateActive(); ticking = false; });
+      ticking = true;
+    }
+  });
+  updateActive();
+})();
